760b21a77baef47b0685bd7b75ec2c5f303e1bf4
Muaadh Melhi <Muaadhmelhi@ac.c4q.nyc>
2017-11-20 19:58:13 -0500

    First

---
+++ app/src/main/java/nyc/muaadh_melhi_develpoer/unite3_mid_my_review/MainActivity.java
@@ -0,0 +1,39 @@
+package nyc.muaadh_melhi_develpoer.unite3_mid_my_review;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+}
